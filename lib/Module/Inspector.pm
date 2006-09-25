e26dcc85d976deb480097c92c574cc0a1b3e41d8
Adam Kennedy <adamk@cpan.org>
2006-09-25 10:10:55 +0000

    Import of ADAMK/Module-Inspector-0.02 from CPAN.
    gitpan-cpan-distribution: Module-Inspector
    gitpan-cpan-version:      0.02
    gitpan-cpan-path:         ADAMK/Module-Inspector-0.02.tar.gz
    gitpan-cpan-author:       ADAMK
    gitpan-cpan-maturity:     released

--- lib/Module/Inspector.pm
+++ lib/Module/Inspector.pm
@@ -46,6 +46,7 @@ inside a tarball.
 use 5.005;
 use strict;
 use Carp                   ();
+use version                ();
 use File::Spec             ();
 use File::Path             ();
 use File::Temp             ();
@@ -53,23 +54,55 @@ use File::Find::Rule       ();
 use File::Find::Rule::VCS  ();
 use File::Find::Rule::Perl ();
 use Params::Util           ('_STRING');
+use Module::Manifest       ();
+use Module::Math::Depends  ();
+use YAML::Tiny             ();
 
-use vars qw{$VERSION};
+use vars qw{$VERSION %SPECIAL};
 BEGIN {
-	$VERSION = '0.01';
+	$VERSION = '0.02';
+	%SPECIAL = (
+		'MANIFEST' => 'Module::Manifest',
+		'META.yml' => 'YAML::Tiny',
+		);
 }
 
 # If prefork is available, flag the optional modules
-eval " use prefork 'YAML::Tiny';          ";
-eval " use prefork 'Archive::Extract';    ";
-eval " use prefork 'PPI::Document::File'; ";
-
+eval " use prefork 'Archive::Extract';      ";
+eval " use prefork 'PPI::Document::File';   ";
 
 
 
 
 #####################################################################
 # Constructor
+
+=pod
+
+=head2 new
+
+  # Inspect a plain dist directory or cvs/svn checkout
+  my $dir = Module::Inspector->new(
+          dist_dir => $dirpath,
+          );
+  
+  # Inspect a tarball
+  my $file = Module::Inspector->new(
+          dist_file => 'Foo-Bar-0.01.tar.gz',
+          );
+
+The C<new> constructor creates a new module inspector. It takes a
+named param of either C<dist_file>, which should be the file path
+of the dist tarball, or C<dist_dir>, which is the root of the
+distribution directory (if it is already unrolled).
+
+The distribution will be quickly pre-scanned to locate the various
+significant documents in the distribution (although only a few are
+initially supported).
+
+Returns a new C<Module::Inspector> object, or dies on exception.
+
+=cut
 
 sub new {
 	my $class = shift;
@@ -145,9 +178,10 @@ sub new {
 	# Create the document store
 	$self->{document} = {};
 
-	# Add the META.yml file to the document store
-	if ( -e $self->file_path('META.yml') ) {
-		$self->{document}->{'META.yml'} = 'YAML::Tiny';
+	# Add all single special files to the document store
+	foreach my $file ( sort keys %SPECIAL ) {
+		next unless -f $self->file_path($file);
+		$self->{document}->{$file} = $SPECIAL{$file};
 	}
 
 	# Populate the document store with all Perl files
@@ -159,27 +193,109 @@ sub new {
 	$self;
 }
 
+=pod
+
+=head2 dist_file
+
+The C<dist_file> accessor returns the (absolute) path to the
+distribution tarball. If the inspector was created with C<dist_dir>
+rather than C<dist_file>, this will return C<undef>.
+
+=cut
+
 sub dist_file {
 	$_[0]->{dist_file};
 }
+
+=pod
+
+=head2 dist_file_type
+
+The C<dist_file_type> method returns the archive type of the
+distribution tarball. This will be either 'tar.gz', 'tgz', or
+'zip'. Other file types are not supported.
+
+If the inspector was created with C<dist_dir> rather than 
+C<dist_file>, this will return C<undef>.
+
+=cut
 
 sub dist_file_type {
 	$_[0]->{dist_file_type};
 }
 
+=pod
+
+=head2 dist_dir
+
+The C<dist_dir> method returns the (absolute) distribution root directory.
+
+If the inspector was created with C<dist_file> rather than C<dist_file>,
+this method will return the temporary directory created to hold the
+unwrapped tarball.
+
+=cut
+
 sub dist_dir {
 	$_[0]->{dist_dir};
 }
+
+=pod
+
+=head2 file_path
+
+  my $local_path = $inspector->file_path('lib/Foo.pm');
+
+To simplify implementations, most tools that work with distributions
+identify files in unix-style relative paths.
+
+The C<file_path> method takes a unix-style relative path and returns
+a localised absolute path to the file on disk (either in the actual
+distribution directory, or the temp directory holding the expanded
+tarball.
+
+=cut
 
 sub file_path {
 	File::Spec->catfile( $_[0]->dist_dir, $_[1] );
 }
 
+=pod
+
+=head2 dir_path
+
+  my $local_path = $inspector->file_path('lib');
+
+The C<dir_path> method is the matching pair of the C<file_path> method.
+
+As for that method, it takes a unix-style relative directory name,
+and returns a localised absolute path to the directory.
+
+=cut
+
 sub dir_path {
 	File::Spec->catdir( $_[0]->dist_dir, $_[1] );
 }
 
-# Detect version control
+=pod
+
+=head2 version_control
+
+  my $vcs_type = $self->version_control;
+
+For reasons that will hopefully be more apparant later,
+B<Module::Inspector> detects any version control system
+in use within the C<dist_dir> for the module.
+
+Currently, support is limited to detection of CVS and
+Subversion.
+
+Returns a the name of the version control system detected in use
+as a string (currently 'cvs' or 'svn'). If no version control is
+able to be detected returns the null string ''.
+
+=cut
+
 sub version_control {
 	my $self = shift;
 
@@ -202,12 +318,45 @@ sub version_control {
 	$self->{version_control};
 }
 
-# The list of documents
+=head2 documents
+
+The C<documents> method returns a list of the names of all the documents
+detected by the C<Module::Inspector>.
+
+In scalar context, returns the number of identifyable documens found in
+the distribution.
+
+=cut
+
 sub documents {
-	sort keys %{ $_[0]->{document} };
+	if ( wantarray ) {
+		return sort keys %{ $_[0]->{document} };
+	} else {
+		return scalar keys %{ $_[0]->{document} };
+	}
 }
 
-# The type of document
+=pod
+
+=head2 document_type
+
+  # Returns 'PPI::Document::File'
+  my $ppi_class = $inspector->document_type('lib/Foo.pm');
+
+In B<Module::Inspector>, all documents are represented as objects.
+
+Thus, for each different type of document, there is going to be a
+different class that implements the document objects for that type.
+
+The C<document_type> method returns the type for a provided document
+as a class name.
+
+Please note that at this time these document types are not necesarily
+stable, and over the first several releases I may need to change
+the class I'm using to represent a particular document type.
+
+=cut
+
 sub document_type {
 	my $self = shift;
 	my $file = _STRING(shift)
@@ -218,7 +367,24 @@ sub document_type {
 	ref($self->{document}->{$file}) or $self->{document}->{$file};
 }
 
-# Return the document, loading it if needed
+=pod
+
+=head2 document
+
+  my $perl = $inspector->document('lib/Foo.pm');
+
+The C<document> method returns the document object for a named file,
+loading and caching it on the fly if needed.
+
+The type of object will vary depending on the document.
+
+For example, a Perl file will be returned as a L<PPI::Document>,
+a F<MANIFEST> file as a L<Module::Manifest>, and so on.
+
+Returns an object, or dies on error.
+
+=cut
+
 sub document {
 	my $self = shift;
 	my $file = _STRING(shift)
@@ -242,8 +408,12 @@ sub document {
 		$self->{document}->{$file} = $document;
 
 	} elsif ( $loader eq 'YAML::Tiny' ) {
-		require	YAML::Tiny;
 		my $document = YAML::Tiny->read( $path )
+			or Carp::croak("Failed to load $file with $loader");
+		$self->{document}->{$file} = $document;
+
+	} elsif ( $loader eq 'Module::Manifest' ) {
+		my $document = Module::Manifest->new( $path )
 			or Carp::croak("Failed to load $file with $loader");
 		$self->{document}->{$file} = $document;
 
@@ -259,18 +429,127 @@ sub document {
 
 
 #####################################################################
-# Installer Detection
+# Analysis Layer
 
-sub makefile_pl {
-	shift->document('Makefile.PL');
+=pod
+
+=head2 dist_name
+
+  # Returns Config-Tiny
+  my $name = $inspector->dist_name;
+
+The C<dist_name> method returns the name of the distribution, as
+determined from the META.yml file.
+
+Returns the name as a string, or dies on error.
+
+=cut
+
+sub dist_name {
+	my $self = shift;
+	my $meta = $self->document('META.yml');
+	$meta->[0]->{name} or Carp::croak(
+		"META.yml does not have a name: value"
+		);
 }
 
-sub build_pl {
-	shift->document('Build.PL');
+=pod
+
+=head2 dist_version
+
+The C<dist_version> method returns the version of the distribution, as
+determined from the F<META.yml> file in the distribution.
+
+Returns a L<version> object, or dies on error.
+
+=cut
+
+sub dist_version {
+	my $self    = shift;
+	my $meta    = $self->document('META.yml');
+	my $version = $meta->[0]->{version}
+		or Carp::croak("META.yml does not have a version: value");
+	version->new($version);
+}
+
+=pod
+
+=head2 dist_requires
+
+  my $depends = $inspector->dist_requires;
+
+The C<dist_requires> method checks for any run-time dependencies of the
+distribution and returns them as a L<Module::Math::Depends> object.
+
+See the docs for L<Module::Math::Depends> for more information on its
+structure and API.
+
+If the distribution has no run-time dependencies, the object will still
+be returned, but will be empty.
+
+Returns a single L<Module::Math::Depends> object, or dies on error.
+
+=cut
+
+sub dist_requires {
+	my $self     = shift;
+	my $meta     = $self->document('META.yml');
+	my $requires = $meta->[0]->{requires};
+	return $requires
+		? Module::Math::Depends->from_hash( $requires )
+		: Module::Math::Depends->new;
+}
+
+=pod
+
+=head2 dist_build_requires
+
+The C<dist_build_requires> method returns the build-time-only
+dependencies of the distribution.
+
+If there are no build-time dependencies, the object will still
+be returned, but will be empty.
+
+Returns a L<Module::Math::Depends> object, or dies on exception.
+
+=cut
+
+sub dist_build_requires {
+	my $self     = shift;
+	my $meta     = $self->document('META.yml');
+	my $requires = $meta->[0]->{build_requires} or return {};
+	return $requires
+		? Module::Math::Depends->from_hash( $requires )
+		: Module::Math::Depends->new;
+}
+
+=pod
+
+=head2 dist_depends
+
+The C<dist_depends> method returns as for the two methods above
+(C<dist_requires> and C<dist_build_requires>) except that this
+method returns a merged dependency object, representing BOTH the
+install-time and run-time dependencies for the distribution.
+
+If there are no build-time or run-time dependencies, the object
+will be returned, but will be empty.
+
+Returns a L<Module::Math::Depends> object, or dies on error.
+
+=cut
+
+sub dist_depends {
+	my $self           = shift;
+	my $requires       = $self->dist_requires;
+	my $build_requires = $self->dist_build_requires;
+	$requires->merge( $build_requires );
+	return $requires;
 }
 
 1;
 
+=pod
 
 =head1 TO DO
 
@@ -303,13 +582,6 @@ L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=Module-Inspector>
 For other issues, for commercial enhancement or support, or to have your
 write access enabled for the repository, contact the author at the email
 address above.
-
-=head1 ACKNOWLEDGEMENTS
-
-The biggest acknowledgement must go to Chris Nandor, who wielded his
-legendary Mac-fu and turned my initial fairly ordinary Darwin
-implementation into something that actually worked properly everywhere,
-and then donated a Mac OS X license to allow it to be maintained properly.
 
 =head1 AUTHORS
 
